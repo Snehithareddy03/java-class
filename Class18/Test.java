cb9c74cf8bd54089295d174a16f7321f46bfcfdf
snehi <snehitha2703@gmail.com>
2025-12-03 12:22:49 +0530

    updated

---
+++ Class18/Test.java
@@ -0,0 +1,10 @@
+class Test{
+    Test(int a,int b){
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+        new Test(10,20);
+        new Test(40,50);
+        new Test(100,200);
+    }
+}
